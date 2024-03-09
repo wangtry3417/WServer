d3e84fff8f6a75a6438c6cfe97835f59576a9825
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-03-09 13:08:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ io.on("connection", (socket) => {
      socket.join("wtech-001");
      io.to("wtech-001").emit("welcome","Hello,you are in wtech-001 road.");
    });
+   socket.on("RoadTwo",() => {
+     socket.join("wtech-002");
+      let device = {
+  state: 'off', // 初始狀態
+  toggle: function() { // 切換狀態的方法
+    this.state = (this.state === 'off') ? 'on' : 'off';
+    console.log('Device is now ' + this.state);
+  }
+}
+     io.to("wtech-002").emit("state",device.state);
+   });
 });
