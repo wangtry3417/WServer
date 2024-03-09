f5ba3e3f210346c8f4c8bc65deda246c9f2f426a
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-03-09 08:35:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const io = new Server(3000);
 io.on("connection", (socket) => {
    socket.on("RoadOne",() => {
      socket.join("wtech-001");
+     io.to("wtech-001").emit("Hello","there");
    });
    io.to("wtech-001").emit("welocme","Hello");
 });
