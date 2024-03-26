8081de3ca4623bca4fbbd89da046c7bb09909ca0
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-03-26 21:17:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const io = new Server(3000);
 
 io.on("connection", (socket) => {
    socket.emit("con","hello");
+   socket.on("join", function(room, username){
+    if (username != ""){
+      rooms[socket.id] = room;
+      usernames[socket.id] = username;
+      socket.leaveAll();
+      socket.join(room);
+      io.in(room).emit("recieve", "Server : " + username + " has entered the chat.");
+      socket.emit("join", room);
+    }
+  })
+
+  socket.on("send", function(message){
+    io.in(rooms[socket.id]).emit("recieve", usernames[socket.id] +" : " + message);
+  })
+
+  socket.on("recieve", function(message){
+    socket.emit("recieve", message);
+  })
    socket.on("RoadOne",() => {
      socket.join("wtech-001");
      io.to("wtech-001").emit("welcome","Hello,you are in wtech-001 road.");
