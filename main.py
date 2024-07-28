3a1fb1453cca4ad8a38b9918910e693416f965e2
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-07-28 14:35:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import send_file,Flask,render_template
-
+from wcloud import createServer
 
 app = Flask(__name__)
+# 創建服務器
+server = createServer(host="0.0.0.0", port=3550)
 
 @app.route("/")
 def index():
