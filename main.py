458064c638eb2ff15f031c44fa55d5a2bc284f64
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-07-30 10:51:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import send_file,Flask,render_template
+from flask import send_file,Flask,render_template,redirect
 from wcloud import createServer
 
 app = Flask(__name__)
@@ -7,6 +7,15 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
+@app.route("/stop")
+def stop():
+  server.stop_container()
+  return redirect("/")
+
+@app.route("/files")
+def get_files():
+  file_list = server.get_file_list()
+  return render_template("files.html",files=file_list)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",port=5000)
