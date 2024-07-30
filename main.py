ac768f376c79e832650bbc9f5b933bb33a40f128
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-07-30 11:01:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from wcloud import createServer
 
 app = Flask(__name__)
 
+@app.before_request
+def create_server():
+  global server
+  # 創建wcloud服務器
+  server = createServer(host="0.0.0.0", port=3550)
+
 @app.route("/")
 def index():
   return render_template("index.html")
@@ -26,4 +32,4 @@ def download_file(filename):
 if __name__ == "__main__":
   app.run(host="0.0.0.0",port=5000)
   # 創建wcloud服務器
-  server = createServer(host="0.0.0.0", port=3550)
+  # server = createServer(host="0.0.0.0", port=3550)
